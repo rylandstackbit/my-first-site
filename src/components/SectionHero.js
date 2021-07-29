457bbbc75ae90579655657e3b900749bdcdf66b8
Stackbit Code Editor <projects@stackbit.com>
2021-07-29 21:20:09 +0000

    SectionHero.js: changed

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -24,6 +24,7 @@ export default function SectionHero(props) {
                         </div>
                     )}
                     <div className="cell block-content">
+                        <small>
                         {title && <h2 className="block-title underline">{title}</h2>}
                         {content && (
                             <div className="block-copy">
