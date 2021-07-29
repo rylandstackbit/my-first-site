164831586ef62543dea1ccea8168cdeca7fe1d9a
Stackbit Code Editor <projects@stackbit.com>
2021-07-29 21:37:20 +0000

    SectionHero.js: changed

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -13,6 +13,7 @@ export default function SectionHero(props) {
     const title = _.get(section, 'title');
     const content = _.get(section, 'content');
     const actions = _.get(section, 'actions');
+    const eyebrow = _.get(section, 'eyebrow');
 
     return (
         <section id={sectionId} className="block hero-block bg-accent outer">
@@ -24,6 +25,7 @@ export default function SectionHero(props) {
                         </div>
                     )}
                     <div className="cell block-content">
+                        <small>{eyebrow}</small>
                         {title && <h2 className="block-title underline">{title}</h2>}
                         {content && (
                             <div className="block-copy">
